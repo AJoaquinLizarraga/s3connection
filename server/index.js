91b771a96a52f80311c4bd52af46e20b3166b8f6
AJoaquinLizarraga <joaem.lizarraga@gmail.com>
2023-08-30 12:58:28 -0300

    function upload download getFiles getFile getFileURL, falta deleteFiles

--- server/index.js
+++ server/index.js
@@ -2,25 +2,29 @@ import "./s3.js";
 import express from "express";
 import fileUpload from "express-fileupload";
 import morgan from "morgan";
-import { uploadFile } from "./s3.js";
+import {
+  uploadFile,
+  getFiles,
+  getFile,
+  downloadFile,
+  getFileURL,
+  deleteFile,
+} from "./s3.js";
 
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Server listening at port 3001 ");
-});
 app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 app.use(
   fileUpload({
@@ -29,12 +33,66 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.json({ msg: "hola" });
+app.get("/files", async (req, res) => {
+  try {
+    const files = await getFiles();
+    res.json(files.Contents);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.get("/files/:fileName", async (req, res) => {
+  try {
+    const file = await getFile(req.params.fileName);
+    const fileData = {
+      ETag: file.ETag,
+      contentType: file.ContentType,
+    };
+    res.json(fileData);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.get("/files/download/:fileName", async (req, res) => {
+  try {
+    await downloadFile(req.params.fileName);
+    res.json({ message: "File download" });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.get("/files/url/:fileName", async (req, res) => {
+  try {
+    const fileUrl = await getFileURL(req.params.fileName);
+    res.json({
+      url: fileUrl,
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.delete("/files/:fileName", async (req, res) => {
+  try {
+    await deleteFile(req.params.fileName);
+    res.json({ message: "File deleted" });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.post("/files", async (req, res) => {
-  console.log(req.files);
-  await uploadFile(req.files.nuevaImagen);
-  res.json({ msg: "hola" });
+  try {
+    const result = await uploadFile(req.files.newImage);
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server listening at port 3001 ");
 });
--- server/s3.js
+++ server/s3.js
@@ -1,43 +1,12 @@
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-// import fs from "fs";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-// const {
-//   AWS_REGION,
-//   AWS_ACCESS_KEYID,
-//   AWS_SECRET_ACCESS_KEYID,
-//   AWS_BUCKET_NAME,
-// } = process.env;
-
-// const client = new S3Client({
-//   region: AWS_REGION,
-//   credentials: {
-//     awsAccessKeyId: AWS_ACCESS_KEYID,
-//     awsSecretAccessKeyId: AWS_SECRET_ACCESS_KEYID,
-//   },
-// });
-
-// const uploadFile = async (file) => {
-//   console.log(file?.tempFilePath);
-//   const stream = fs.createReadStream(file?.tempFilePath);
-//   const uploadParams = {
-//     Bucket: "blog-images-bocancorp",
-//     Key: "imagenPrueba",
-//     body: stream,
-//   };
-
-//   const command = new PutObjectCommand(uploadParams);
-//   const result = await client.send(command);
-//   console.log(result);
-// };
-
-// export { uploadFile };
-
-import AWS from "aws-sdk";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+  ListObjectsV2Command,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
 import fs from "fs";
 import dotenv from "dotenv";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 dotenv.config();
 const {
@@ -47,40 +16,74 @@ const {
   AWS_BUCKET_NAME,
 } = process.env;
 
-const s3 = new AWS.S3({
-  accessKeyId: AWS_ACCESS_KEYID,
-  secretAccessKey: AWS_SECRET_ACCESS_KEYID,
+const client = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEYID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEYID,
+  },
 });
 
-// const client = new S3Client({
-//   region: AWS_REGION,
-//   credentials: {
-//     awsAccessKeyId: AWS_ACCESS_KEYID,
-//     awsSecretAccessKeyId: AWS_SECRET_ACCESS_KEYID,
-//   },
-// });
-
 const uploadFile = async (file) => {
-  console.log(file?.tempFilePath);
-  const stream = fs.readFileSync(file?.tempFilePath);
-  const uploadParams = s3.upload(
-    {
-      Bucket: "blog-images-bocancorp",
-      Key: file?.tempFilePath,
-      Body: stream,
-    },
-    (error, data) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(`File uploaded successfully. ${data.Location}`);
-      }
-    }
-  );
+  try {
+    const stream = fs.createReadStream(file?.tempFilePath);
 
-  // const command = new PutObjectCommand(uploadParams);
-  // const result = await client.send(command);
-  // console.log(result);
+    const uploadParams = {
+      Bucket: AWS_BUCKET_NAME,
+      Key: file.name,
+      Body: stream,
+    };
+    const command = new PutObjectCommand(uploadParams);
+    return await client.send(command);
+  } catch (error) {
+    throw error({ error: error.message });
+  }
 };
 
-export { uploadFile };
+const getFiles = async () => {
+  try {
+    const command = new ListObjectsV2Command({
+      Bucket: AWS_BUCKET_NAME,
+    });
+    return await client.send(command);
+  } catch (error) {
+    throw error({ error: error.message });
+  }
+};
+
+const getFile = async (filename) => {
+  try {
+    console.log(filename);
+    const command = new GetObjectCommand({
+      Bucket: AWS_BUCKET_NAME,
+      Key: filename,
+    });
+    return await client.send(command);
+  } catch (error) {
+    throw error({ error: error.message });
+  }
+};
+
+const downloadFile = async (filename) => {
+  try {
+    console.log(filename);
+    const command = new GetObjectCommand({
+      Bucket: AWS_BUCKET_NAME,
+      Key: filename,
+    });
+    const result = await client.send(command);
+    result.Body.pipe(fs.createWriteStream(`./images/${filename}`));
+  } catch (error) {
+    throw error({ error: error.message });
+  }
+};
+const getFileURL = async (filename) => {
+  const command = new GetObjectCommand({
+    Bucket: AWS_BUCKET_NAME,
+    Key: filename,
+  });
+  return await getSignedUrl(client, command, { expiresIn: 120 });
+};
+const deleteFile = () => {};
+
+export { uploadFile, getFiles, getFile, downloadFile, getFileURL, deleteFile };
