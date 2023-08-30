47a2d67e218ad6e263dd92916a1590cf8339ded2
AJoaquinLizarraga <joaem.lizarraga@gmail.com>
2023-08-30 09:38:06 -0300

    upload images works

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import "./s3.js";
 import express from "express";
 import fileUpload from "express-fileupload";
 import morgan from "morgan";
+import { uploadFile } from "./s3.js";
 
 const app = express();
 
@@ -9,13 +10,31 @@ app.listen(3001, () => {
   console.log("Server listening at port 3001 ");
 });
 app.use(morgan("dev"));
-app.use(fileUpload());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ msg: "hola" });
 });
 
-app.post("/files", () => {
+app.post("/files", async (req, res) => {
   console.log(req.files);
+  await uploadFile(req.files.nuevaImagen);
   res.json({ msg: "hola" });
 });
--- server/s3.js
+++ server/s3.js
@@ -1,6 +1,43 @@
+// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+// import fs from "fs";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// const {
+//   AWS_REGION,
+//   AWS_ACCESS_KEYID,
+//   AWS_SECRET_ACCESS_KEYID,
+//   AWS_BUCKET_NAME,
+// } = process.env;
+
+// const client = new S3Client({
+//   region: AWS_REGION,
+//   credentials: {
+//     awsAccessKeyId: AWS_ACCESS_KEYID,
+//     awsSecretAccessKeyId: AWS_SECRET_ACCESS_KEYID,
+//   },
+// });
+
+// const uploadFile = async (file) => {
+//   console.log(file?.tempFilePath);
+//   const stream = fs.createReadStream(file?.tempFilePath);
+//   const uploadParams = {
+//     Bucket: "blog-images-bocancorp",
+//     Key: "imagenPrueba",
+//     body: stream,
+//   };
+
+//   const command = new PutObjectCommand(uploadParams);
+//   const result = await client.send(command);
+//   console.log(result);
+// };
+
+// export { uploadFile };
+
+import AWS from "aws-sdk";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import dotenv from "dotenv";
 import fs from "fs";
+import dotenv from "dotenv";
 
 dotenv.config();
 const {
@@ -10,25 +47,40 @@ const {
   AWS_BUCKET_NAME,
 } = process.env;
 
-const client = new S3Client({
-  region: AWS_REGION,
-  credentials: {
-    awsAccessKeyId: AWS_ACCESS_KEYID,
-    awsSecretAccessKeyId: AWS_SECRET_ACCESS_KEYID,
-  },
+const s3 = new AWS.S3({
+  accessKeyId: AWS_ACCESS_KEYID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEYID,
 });
 
-const uploadFile = async (file) => {
-  const stream = fs.createReadStream(file);
-  const uploadParams = {
-    Bucket: AWS_BUCKET_NAME,
-    Key: "nombre del archivo.png",
-    body: stream,
-  };
+// const client = new S3Client({
+//   region: AWS_REGION,
+//   credentials: {
+//     awsAccessKeyId: AWS_ACCESS_KEYID,
+//     awsSecretAccessKeyId: AWS_SECRET_ACCESS_KEYID,
+//   },
+// });
 
-  const command = new PutObjectCommand(uploadParams);
-  const result = await client.send(command);
-  console.log(result);
+const uploadFile = async (file) => {
+  console.log(file?.tempFilePath);
+  const stream = fs.readFileSync(file?.tempFilePath);
+  const uploadParams = s3.upload(
+    {
+      Bucket: "blog-images-bocancorp",
+      Key: file?.tempFilePath,
+      Body: stream,
+    },
+    (error, data) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`File uploaded successfully. ${data.Location}`);
+      }
+    }
+  );
+
+  // const command = new PutObjectCommand(uploadParams);
+  // const result = await client.send(command);
+  // console.log(result);
 };
 
 export { uploadFile };
